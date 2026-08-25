ec7df2539cfcc633eec6323d111d0b1f3e40db2c
Vignesh Venkatesh <vigneshvenkatesh2k3@gmail.com>
2026-08-25 12:31:04 -0400

    feat: shortcut to toggle

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,48 @@
+use tauri::Manager;
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// startup function
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        // install global shortcut plugin
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new()
+                .with_handler(|app, _shortcut, event| {
+                    if event.state == ShortcutState::Pressed {
+                        if let Some(window) = app.get_webview_window("main") {
+                            if window.is_visible().unwrap_or(false) {
+                                let _ = window.hide();
+                            } else {
+                                let _ = window.show();
+                                let _ = window.set_focus();
+                            }
+                        }
+                    }
+                })
+                .build(),
+        )
+        // install opener plugin
         .plugin(tauri_plugin_opener::init())
+        // making greet function callable from frontend
         .invoke_handler(tauri::generate_handler![greet])
+        // runs during startup
+        .setup(|app| {
+            // shortcut
+            let shortcut = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::ALT), Code::Space);
+
+            // registering shortcut globally
+            app.global_shortcut().register(shortcut)?;
+            Ok(())
+        })
+        // launching the app
         .run(tauri::generate_context!())
+        // crash message
         .expect("error while running tauri application");
 }
